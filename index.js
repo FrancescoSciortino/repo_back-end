45c450fb2b93737e14e667fa7ce143681fa8d782
Francesco <francesco.sciortino04@community.unipa.it>
2020-10-30 11:57:53 +0100

    fail commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+if (port == null || port == "") {
+    port = 8000;
+  }
 
 app.use(express.static(__dirname + '/public'));
-
+window.open("index.html")
 app.listen(port);
